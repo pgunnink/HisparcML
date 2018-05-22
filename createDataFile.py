557445d40f95ef432ce6ad87a7f0065b7226fa6d
pgunnink <38908229+pgunnink@users.noreply.github.com>
2018-05-22 11:54:04 +0200

    Added ability to not calculate MIPS

--- createDataFile.py
+++ createDataFile.py
@@ -1,6 +1,7 @@
 from ProcessData import read_sapphire_simulation
 
 
-H5FILE = "C:\\Users\\piete\\OneDrive\\Documenten\\Nikhef\\Sims\\drieehoek225allezeniths.h5"
+H5FILE = "C:\\Users\\piete\\OneDrive\\Documenten\\Nikhef\\Losse " \
+         "sims\\driehoekzonderfilter.h5"
 new_H5FILE = "C:\\Users\\piete\\OneDrive\\Documenten\\Nikhef\\ML\\driehoek.h5"
-read_sapphire_simulation(H5FILE, new_H5FILE, 3)
+read_sapphire_simulation(H5FILE, new_H5FILE, 3, find_mips=False)
