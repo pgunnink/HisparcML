ac97032e3f1a49a9925663ced8cf380c7608a390
pgunnink <38908229+pgunnink@users.noreply.github.com>
2018-05-24 10:01:10 +0200

    Package changes

--- createDataFile.py
+++
@@ -1,7 +0,0 @@
-from ProcessData import read_sapphire_simulation
-
-
-H5FILE = "C:\\Users\\piete\\OneDrive\\Documenten\\Nikhef\\Losse sims" \
-         "\\main_data_[503_504_506].h5"
-new_H5FILE = "C:\\Users\\piete\\OneDrive\\Documenten\\Nikhef\\ML\\driehoek.h5"
-read_sapphire_simulation(H5FILE, new_H5FILE, 3, find_mips=True, uniform_dist=True)
