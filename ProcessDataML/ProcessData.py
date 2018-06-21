0446cf9defe83d57c9058f9e5087698d575a24d0
pgunnink <38908229+pgunnink@users.noreply.github.com>
2018-06-21 16:45:57 +0200

    Changed timings histogram bins

--- ProcessDataML/ProcessData.py
+++ ProcessDataML/ProcessData.py
@@ -287,7 +287,8 @@ def read_sapphire_simulation(file_location, new_file_location, N_stations,
             timings -= np.nanmean(timings,axis=1)[:,np.newaxis]
             if verbose:
                 plt.figure()
-                plt.hist(np.extract(~np.isnan(timings.flatten()),timings.flatten()),bins=30)
+                plt.hist(np.extract(~np.isnan(timings.flatten()),timings.flatten()),
+                         bins=np.linspace(-100,100,50))
                 plt.savefig('histogram_timings.png')
             print('Std of timings: %s' % np.nanstd(timings))
             timings /= np.nanstd(timings)
